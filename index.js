2186f9a12c603db58d11c91b5c56890eef0a2ef1
ZiZi07 <ashfakurriaz@gmail.com>
2024-10-22 16:27:33 +0530

    changes for deployment

--- index.js
+++ index.js
@@ -1,23 +1,32 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import saveMessageRouter from './routes/portfolio.routes.js';
-import cors from 'cors';
+import express from "express";
+import dotenv from "dotenv";
+import saveMessageRouter from "./routes/portfolio.routes.js";
+import cors from "cors";
+import path from "path";
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+const __dirname = path.resolve();
+
 app.use(
-    cors({
-      origin: "http://localhost:5173",
-      credentials: true,
-    })
-  );
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 app.use("/api", saveMessageRouter);
 
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
+
 app.listen(port, () => {
-    console.log('Server started on port ', port);
+  console.log("Server started on port ", port);
 });
